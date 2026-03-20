e89a04dd1f57c1a9b164fce72703b6ff68271c35
ZelalDev <zelall2222@gmail.com>
2026-03-20 16:22:59 +0300

    Initial Commit

---
+++ Menu81/Menu81App.swift
@@ -0,0 +1,17 @@
+//
+//  Menu81App.swift
+//  Menu81
+//
+//  Created by Zelal Aydın on 20.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Menu81App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Menu81Tests/Menu81Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Menu81Tests.swift
+//  Menu81Tests
+//
+//  Created by Zelal Aydın on 20.03.2026.
+//
+
+import Testing
+@testable import Menu81
+
+struct Menu81Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
